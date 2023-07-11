3783db21a87eeecbedb8ff50bedb85db98d29e8b
Justin Ramirez <justinramirez2010@hotmail.com>
2023-07-11 16:21:46 -0700

    updated web scraper

--- web_scraper.py
+++ web_scraper.py
@@ -1,5 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
+# instead of using .storyline use .titleline.
+
 res = requests.get('https://news.ycombinator.com/news')
-print(res.text)
+# here we grave the html info from the website.
+soup = BeautifulSoup(res.text, "html.parser")
+
+# 'a' tags are all the links, the '#' symbol stands for 'id', '.' means it's a class
+links = soup.select('.titleline > a')
+# print(links)
+votes = soup.select('.score')
+
+
+
+def create_custom_hn(links, votes):  # a link in html id 'href'
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href')
+        hn.append({'link': href, 'title': title})
+
+    return hn
+
+
+print(create_custom_hn(links, votes))
